4392190f6b859951010d2e8bf41ba0b56c02b8e0
clarklindev <clarklindev@gmail.com>
2025-06-20 14:34:31 +0800

    add to i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,12 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
+    lng: "zh-TW",
     fallbackLng: "en",
+    ns: ["translation"],
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
